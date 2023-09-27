7632c6befbcb4f61d39c0722e6c07f4fffdf9566
Chris Hall <christopher.k.hall@gmail.com>
2023-09-27 00:10:17 +0000

    import

--- variables.tf
+++ variables.tf
@@ -7,3 +7,13 @@ variable "user_uuid" {
     error_message = "The input is not a valid UUID. Please provide a valid UUID."
   }
 }
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+
+  validation {
+    condition = can(regex("^[a-z0-9.-]{3,63}$", var.bucket_name))
+    error_message = "Bucket name must be between 3 and 63 characters, lowercase, and can only contain letters, numbers, hyphens, and periods. It can't start or end with a hyphen or period, and can't have consecutive hyphens or periods."
+  }
+}
