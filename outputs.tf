f606defdcea9badb44c92cd4929c367d97447b43
Chris Hall <christopher.k.hall@gmail.com>
2023-09-30 13:18:50 +0000

    invalidate cloudfront stack when content version changes

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "website_endpoint" {
   description = "website endpoint"
   value = module.terrahouse_aws.website_endpoint
 }
+
+output "cloudfront_url" {
+  description = "cloudfront url"
+  value = module.terrahouse_aws.cloudfront_url
+}
