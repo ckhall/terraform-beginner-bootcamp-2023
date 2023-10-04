7541f779e79a2ee32c4aae2d8b3961515ab62e03
Chris Hall <christopher.k.hall@gmail.com>
2023-10-04 22:16:46 +0000

    implement terraform provider block

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "bucket_name" {
-  description = "bucket name for our static website hosting"
-  value = module.terrahouse_aws.bucket_name
-}
+# output "bucket_name" {
+#   description = "bucket name for our static website hosting"
+#   value = module.terrahouse_aws.bucket_name
+# }
 
-output "website_endpoint" {
-  description = "website endpoint"
-  value = module.terrahouse_aws.website_endpoint
-}
+# output "website_endpoint" {
+#   description = "website endpoint"
+#   value = module.terrahouse_aws.website_endpoint
+# }
 
-output "cloudfront_url" {
-  description = "cloudfront url"
-  value = module.terrahouse_aws.cloudfront_url
-}
+# output "cloudfront_url" {
+#   description = "cloudfront url"
+#   value = module.terrahouse_aws.cloudfront_url
+# }
